0f5a5cdd8893d5be96b76f82c801b05c3779d2aa
quan005 <ralphealfreeman@gmail.com>
2019-07-21 17:49:51 -0700

    MVP Complete

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,6 +1,16 @@
 /*
   Be sure to import in all of the action types from `../actions`
 */
+import {
+  ADDING_SMURF,
+  ADDING_SMURF_SUCCESS,
+  ADDING_SMURF_FAILED,
+  GET_SMURFS,
+  GET_SMURFS_SUCCESS,
+  GET_SMURFS_FAILED,
+  UPDATE_SMURF,
+  DELETE_SMURF
+} from "../actions";
 
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
@@ -13,6 +23,14 @@
    error: null
  }
 */
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+};
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +39,74 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+export default (state = initialState, action) => {
+  switch (action.type) {
+
+      case GET_SMURFS: {
+          return {
+            ...state,
+            fetchingSmurfs: true
+          }
+      }
+
+      case GET_SMURFS_SUCCESS: {
+          return {
+            ...state,
+            fetchingSmurfs: false,
+            smurfs: action.payload,
+            error: null,
+          }
+      }
+
+      case GET_SMURFS_FAILED: {
+          return {
+            ...state,
+            fetchingSmurfs: false,
+            error: action.payload
+          }
+      }
+
+      case ADDING_SMURF: {
+          return {
+            ...state,
+            addingSmurf: true
+          }
+      }
+
+      case ADDING_SMURF_SUCCESS: {
+        return {
+          ...state,
+          addingSmurf: false,
+          smurfs: action.payload
+        }
+    }
+
+    case ADDING_SMURF_FAILED: {
+        return {
+          ...state,
+          addingSmurf: false,
+          error: action.payload
+        }
+    }
+
+      case UPDATE_SMURF: {
+          return {
+            ...state,
+            friends: action.payload,
+            fetchingFriends: false,
+            error: null,
+          }
+      }
+
+      case DELETE_SMURF: {
+          return {
+            ...state,
+            fetchingFriends: false,
+            error: null,
+          }
+      }
+
+      default:
+          return state;
+  }
+};
